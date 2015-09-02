eba378d595166ccaac08bb3ffb3e23c6d66c133d
Kay Strobach <info@kay-strobach.de>
2015-09-02 13:48:58 +0200

    [TASK] make test executable

--- Tests/Unit/Service/Ldap/PasswordUtilityTest.php
+++ Tests/Unit/Service/Ldap/PasswordUtilityTest.php
@@ -5,6 +5,9 @@ namespace KayStrobach\Ldap\Tests\Unit\Service\Ldap;
 use KayStrobach\Ldap\Service\Ldap\PasswordUtility;
 
 class PasswordUtilityTest extends \TYPO3\Flow\Tests\UnitTestCase {
+	/**
+	 * @test
+	 */
 	public function allHashsReturnedTest() {
 		$this->assertEquals(
 			count(PasswordUtility::$algorithms),
